36631df9c571a7c978b8575040c81a0fba084d37
caileighsmith <62355730+caileighsmith@users.noreply.github.com>
2025-02-20 09:16:31 +0000

    Created landing page for website

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -3,12 +3,43 @@
 export default function Home() {
   return (
     <div className="items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      
+
+
+
+
+    <section class="bg-white dark:bg-gray-900 bg-[url('https://flowbite.s3.amazonaws.com/docs/jumbotron/hero-pattern.svg')] dark:bg-[url('https://flowbite.s3.amazonaws.com/docs/jumbotron/hero-pattern-dark.svg')]">
+        <div class="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 z-10 relative">
+            <a href="#" class="inline-flex justify-between items-center py-1 px-1 pe-4 mb-7 text-sm text-blue-700 bg-blue-100 rounded-full dark:bg-blue-900 dark:text-blue-300 hover:bg-blue-200 dark:hover:bg-blue-800">
+                <span class="text-xs bg-blue-600 rounded-full text-white px-4 py-1.5 me-3">New</span> <span class="text-sm font-medium">Slava Ukraine!</span> 
+                <svg class="w-2.5 h-2.5 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"/>
+                </svg>
+            </a>
+            <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">We help Ukranians <span class="underline underline-offset-3 decoration-8 decoration-blue-400 dark:decoration-blue-600">find jobs</span></h1>
+            <p className="text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400 mb-8">Here at Hire-Ukraine, we connect Ukranians with supporitng businesses for hiring, freelancing and work-oppertunities.</p>
+
+            <form class="w-full max-w-md mx-auto">   
+                <label for="default-email" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Email sign-up</label>
+                <div class="relative">
+                          
+                    <div className="button-container mt-5">
+                      <button type="button" className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">I'm looking for work</button>
+                      <button type="button" className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">For employers</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+        <div class="bg-gradient-to-b from-blue-50 to-transparent dark:from-blue-900 w-full h-full absolute top-0 left-0 z-0"></div>
+    </section>
+
+
+
+
       {/* <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Get back to growth with <span class="text-blue-600 dark:text-blue-500">the world's #1</span> CRM.</h1> */}
-      <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">We help Ukranians <span class="underline underline-offset-3 decoration-8 decoration-blue-400 dark:decoration-blue-600">find jobs</span></h1>
+      {/* <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">We help Ukranians <span class="underline underline-offset-3 decoration-8 decoration-blue-400 dark:decoration-blue-600">find jobs</span></h1>
+      <p className="text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400">Here at Hire-Ukraine, we connect Ukranians with supporitng businesses for hiring, freelancing and work-oppertunities.</p> */}
 
-      <p className="text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400">Here at Hire-Ukraine, we connect Ukranians with supporitng businesses for hiring, freelancing and work-oppertunities.</p>
-
+      
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
       
         {/* Content   */}
