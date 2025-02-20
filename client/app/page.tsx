07368f0a8d9cae5adacc4ac9727ae1d89eb3eae1
caileighsmith <62355730+caileighsmith@users.noreply.github.com>
2025-02-20 11:25:15 +0000

    fixed margin for list of hiring companies

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -21,6 +21,46 @@ const companies = [
   {
     name: "Microsoft",
     hiring: false
+  },{
+    name: "Cisco",
+    hiring: true
+  },
+  {
+
+    name: "Amazon",
+    hiring: true
+  },
+  {
+    name: "Google",
+    hiring: true
+  },
+  {
+    name: "Meta",
+    hiring: true
+  },
+  {
+    name: "Microsoft",
+    hiring: false
+  },{
+    name: "Cisco",
+    hiring: true
+  },
+  {
+
+    name: "Amazon",
+    hiring: true
+  },
+  {
+    name: "Google",
+    hiring: true
+  },
+  {
+    name: "Meta",
+    hiring: true
+  },
+  {
+    name: "Microsoft",
+    hiring: false
   }
 
 ]
@@ -89,8 +129,7 @@ export default function Home() {
           </div>
         </div>
 
-
-<div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+        <div className="relative overflow-x-auto shadow-md sm:rounded-lg mt-10">
     <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
         <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
             <tr>
@@ -99,7 +138,7 @@ export default function Home() {
                 </th>
                 <th scope="col" className="px-6 py-3">
                     <div className="flex items-center">
-                      Hiring (Y/N)
+                      Currently hiring?
                         <a href="#"><svg className="w-3 h-3 ms-1.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
     <path d="M8.574 11.024h6.852a2.075 2.075 0 0 0 1.847-1.086 1.9 1.9 0 0 0-.11-1.986L13.736 2.9a2.122 2.122 0 0 0-3.472 0L6.837 7.952a1.9 1.9 0 0 0-.11 1.986 2.074 2.074 0 0 0 1.847 1.086Zm6.852 1.952H8.574a2.072 2.072 0 0 0-1.847 1.087 1.9 1.9 0 0 0 .11 1.985l3.426 5.05a2.123 2.123 0 0 0 3.472 0l3.427-5.05a1.9 1.9 0 0 0 .11-1.985 2.074 2.074 0 0 0-1.846-1.087Z"/>
   </svg></a>
@@ -131,6 +170,7 @@ export default function Home() {
         </tbody>
     </table>
 </div>
+
 
     </section>
 
