daa2d2d762dec9ae26650949a5214c09cacf7ac1
caileighsmith <62355730+caileighsmith@users.noreply.github.com>
2025-02-20 11:17:37 +0000

    Added company list

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,5 +1,33 @@
 import ciscoLogo from '../public/cisco-logo.png'
 
+const companies = [
+  {
+    name: "Cisco",
+    hiring: true
+  },
+  {
+
+    name: "Amazon",
+    hiring: true
+  },
+  {
+    name: "Google",
+    hiring: true
+  },
+  {
+    name: "Meta",
+    hiring: true
+  },
+  {
+    name: "Microsoft",
+    hiring: false
+  }
+
+]
+
+
+
+
 export default function Home() {
   return (
     <div className="items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
@@ -60,6 +88,53 @@ export default function Home() {
             </dl>
           </div>
         </div>
+
+
+<div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+    <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+        <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+                <th scope="col" className="px-6 py-3">
+                    Company name
+                </th>
+                <th scope="col" className="px-6 py-3">
+                    <div className="flex items-center">
+                      Hiring (Y/N)
+                        <a href="#"><svg className="w-3 h-3 ms-1.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
+    <path d="M8.574 11.024h6.852a2.075 2.075 0 0 0 1.847-1.086 1.9 1.9 0 0 0-.11-1.986L13.736 2.9a2.122 2.122 0 0 0-3.472 0L6.837 7.952a1.9 1.9 0 0 0-.11 1.986 2.074 2.074 0 0 0 1.847 1.086Zm6.852 1.952H8.574a2.072 2.072 0 0 0-1.847 1.087 1.9 1.9 0 0 0 .11 1.985l3.426 5.05a2.123 2.123 0 0 0 3.472 0l3.427-5.05a1.9 1.9 0 0 0 .11-1.985 2.074 2.074 0 0 0-1.846-1.087Z"/>
+  </svg></a>
+                    </div>
+                </th>
+                <th scope="col" className="px-6 py-3">
+                    <div className="flex items-center">
+                        Link to jobs
+                        <a href="#"><svg className="w-3 h-3 ms-1.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
+    <path d="M8.574 11.024h6.852a2.075 2.075 0 0 0 1.847-1.086 1.9 1.9 0 0 0-.11-1.986L13.736 2.9a2.122 2.122 0 0 0-3.472 0L6.837 7.952a1.9 1.9 0 0 0-.11 1.986 2.074 2.074 0 0 0 1.847 1.086Zm6.852 1.952H8.574a2.072 2.072 0 0 0-1.847 1.087 1.9 1.9 0 0 0 .11 1.985l3.426 5.05a2.123 2.123 0 0 0 3.472 0l3.427-5.05a1.9 1.9 0 0 0 .11-1.985 2.074 2.074 0 0 0-1.846-1.087Z"/>
+  </svg></a>
+                    </div>
+                </th>
+                
+            </tr>
+        </thead>
+        <tbody>
+          {companies.map((company, index) => (
+            <tr key={index} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200">
+                <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    {company.name}
+                </th>
+                <td className="px-6 py-4">
+                    {company.hiring ? "Yes" : "No"}
+                </td>
+                <td className="px-6 py-4">
+                    LINKBELONGSHERE
+                </td>
+            </tr>
+          ))}
+            
+        </tbody>
+    </table>
+</div>
+
     </section>
 
 
